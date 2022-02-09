553651be6e94bd8bb20fe21a51c4fd188a48e046
Fiqar Aprialim <fiqarapr@gmail.com>
2022-02-10 02:23:32 +0800

    feat: routes

---
+++ src/routes.js
@@ -0,0 +1,29 @@
+const routes = [
+  {
+    method: 'POST',
+    path: '/books',
+    handler: () => null,
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: () => null,
+  },
+  {
+    method: 'GET',
+    path: '/books/{id}',
+    handler: () => null,
+  },
+  {
+    method: 'PUT',
+    path: '/books/{id}',
+    handler: () => null,
+  },
+  {
+    method: 'DELETE',
+    path: '/books/{id}',
+    handler: () => null,
+  },
+];
+
+export default routes;
