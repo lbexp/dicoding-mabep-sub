bb245a3ed6167856fa33933d8a2e4c6e3b9ea08a
Fiqar Aprialim <fiqarapr@gmail.com>
2022-02-10 02:23:44 +0800

    feat: base config hapi server

--- src/main.js
+++ src/main.js
@@ -1,1 +1,21 @@
-console.log('Main js');
+import Hapi from '@hapi/hapi';
+import routes from './routes.js';
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  server.route(routes);
+
+  await server.start();
+  console.log(`Server sedang berjalan pada ${server.info.uri}`);
+};
+
+init();
